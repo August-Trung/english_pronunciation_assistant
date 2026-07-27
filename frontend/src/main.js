9a3f2066fa5cebe430bf852d7b88929e0e6d932a
August-Trung <nguyenminhtrung01082003@gmail.com>
2026-07-27 23:25:00 +0700

    Fix blank screen issue by removing unfonts virtual module from main.js and vite.config.mjs

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -9,7 +9,6 @@ import { createApp } from 'vue'
 import { registerPlugins } from '@/plugins'
 import router from '@/router'
 import App from './App.vue'
-import 'unfonts.css'
 
 const app = createApp(App)
 registerPlugins(app)
